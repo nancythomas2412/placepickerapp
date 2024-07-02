2a996f7453f85e30eca1edc23740953d7cea3594
Nancy Thomas <nancytreasathomas@gmail.com>
2024-07-02 08:50:31 +0530

    Adds Cleanup function & Optimizes State Updates

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
 
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, (3000));
+    }, TIMER);
 
     return () => { // cleanup function
       clearTimeout(timer);
@@ -25,6 +28,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER}/>
     </div>
   );
 }
